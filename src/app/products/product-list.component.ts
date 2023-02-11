602276ffa78decded9aea484ce7742fba8738631
Yassinmoh <yassin_mohamed310@outlook.com>
2023-02-11 19:32:48 +0200

    create filter - Action Stream

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,10 +1,11 @@
-import { catchError, EMPTY } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, EMPTY, map, Subject } from 'rxjs';
 import { Component, } from '@angular/core';
 
 import { ProductCategory } from '../product-categories/product-category';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -15,15 +16,37 @@ export class ProductListComponent {
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products$ =this.productService.productsWithCategories$.pipe(
-    catchError(err =>{
-      this.errorMessage =err;
+  //crete Action Stream:
+  private categorySelectedSubject = new BehaviorSubject<number>(0)
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable()
+
+
+
+  //combine action and data streams:
+  products$ = combineLatest([
+    this.productService.productsWithCategories$,
+    this.categorySelectedAction$
+  ]).pipe(
+    map(([products, selectedCategoryId]) =>
+      products.filter((product) =>
+        selectedCategoryId ? product.categoryId === selectedCategoryId : true)),
+    catchError(err => {
+      this.errorMessage = err;
       return EMPTY
     })
   )
 
 
-  constructor(private productService: ProductService ) { }
+  categories$ = this.categoryService.categories$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY
+    })
+  )
+
+
+
+  constructor(private productService: ProductService, private categoryService: ProductCategoryService) { }
 
 
 
@@ -31,7 +54,8 @@ export class ProductListComponent {
     console.log('Not yet implemented');
   }
 
+  // Emit value to Action stream when Action is occurred:
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.categorySelectedSubject.next(Number(categoryId))
   }
 }
